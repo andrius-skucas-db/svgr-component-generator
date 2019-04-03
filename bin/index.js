2bb4a0af49f5d56fe7546a07d9c294e2186d3abf
Andrius <andrius.skucas@devbridge.com>
2019-04-03 11:39:23 +0300

    Add bin folder

---
+++ bin/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const minimist = require("minimist");
+const generateComponents = require("../lib/index.js").default;
+
+const args = minimist(process.argv.slice(2));
+
+const config = {
+  source: args.source || "./",
+  dist: args.dist
+};
+
+generateComponents(config);
